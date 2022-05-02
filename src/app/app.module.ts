4bbfb9875eab9456162d2f0c85bc06aac066ad82
gunahusko <gunavvv@gmail.com>
2022-05-02 10:06:48 +0300

    Login-form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlannerSimpleComponent } from './components/planner/planner-simple/planner-simple.component';
-import { FormsModule } from "@angular/forms";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { HeaderComponent } from './components/header/header.component';
 import { PlannerComponent } from './components/planner/planner.component';
 import { PlannerWithCheckboxesComponent } from './components/planner/planner-with-checkboxes/planner-with-checkboxes.component';
 import { PlannerWithCkeckboxesAndFiltersComponent } from './components/planner/planner-with-ckeckboxes-and-filters/planner-with-ckeckboxes-and-filters.component';
 import { PlannerWithDeleteComponent } from './components/planner/planner-with-delete/planner-with-delete.component';
 import { PlannerWithProgressBarComponent } from './components/planner/planner-with-progress-bar/planner-with-progress-bar.component';
+import { FormsComponent } from './components/forms/forms.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component';
 
 @NgModule({
   declarations: [
@@ -20,12 +22,15 @@ import { PlannerWithProgressBarComponent } from './components/planner/planner-wi
     PlannerWithCheckboxesComponent,
     PlannerWithCkeckboxesAndFiltersComponent,
     PlannerWithDeleteComponent,
-    PlannerWithProgressBarComponent
+    PlannerWithProgressBarComponent,
+    FormsComponent,
+    LoginFormComponent
   ],
     imports: [
         BrowserModule,
         AppRoutingModule,
-        FormsModule
+        FormsModule,
+      ReactiveFormsModule
     ],
   providers: [],
   bootstrap: [AppComponent]
